0b1a8d708004be449a92066a238f7d092ffe6edb
C0d3-5t3w <stewy1994@gmail.com>
2025-04-23 08:33:11 -0700

    Update

--- src/web/assets/ts/main.ts
+++ src/web/assets/ts/main.ts
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (heading) {
         heading.addEventListener('click', () => {
-            // Generate a random hex color
             const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
             heading.style.backgroundColor = randomColor;
         });
