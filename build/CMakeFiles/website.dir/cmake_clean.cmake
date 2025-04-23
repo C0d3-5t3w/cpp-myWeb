03170d7f05e86f22b409b4ec160ba41b4d3199a5
C0d3-5t3w <stewy1994@gmail.com>
2025-04-22 22:50:55 -0700

    .

---
+++ build/CMakeFiles/website.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/website.dir/src/main.cpp.o"
+  "CMakeFiles/website.dir/src/main.cpp.o.d"
+  "CMakeFiles/website.dir/src/web/web.cpp.o"
+  "CMakeFiles/website.dir/src/web/web.cpp.o.d"
+  "website"
+  "website.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/website.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
